9da2f4cb46b2b9ef8c5959218d5b3f0ec6df2c2d
Prashant Mahesvara <lord_prashant@hotmail.com>
2018-08-01 13:13:20 +0200

    BootBox installed and working, DataTables functional for delete

--- LinasMovies/LinaMovies.Web/App_Start/BundleConfig.cs
+++ LinasMovies/LinaMovies.Web/App_Start/BundleConfig.cs
@@ -1,16 +1,15 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace LinaMovies.Web
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootbox.js",
                         "~/Scripts/respond.js",
                         "~/Scripts/datatables/jquery.datatables.js",
                         "~/Scripts/datatables/datatables.bootstrap.js"
@@ -20,15 +19,9 @@ namespace LinaMovies.Web
                         "~/Scripts/jquery.validate*"
                         ));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"
                         ));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-      
-                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-cerulean.css",
